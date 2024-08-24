ce9185afccf3917a43ba83b5f9c833926265acd0
Александр Мосин <alexmosin@inbox.ru>
2024-08-24 11:22:23 +0400

    Добавьте файлы проекта.

---
+++ CSharpEducation.GroupProject.ChatMSG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSharpEducation.GroupProject.ChatMSG.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
